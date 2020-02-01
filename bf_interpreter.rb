f4cd8fd3451de088553beda63380dde10760c265
nos <nos@kmc.gr.jp>
2020-02-02 08:53:11 +0900

    ><+-実装?

--- bf_interpreter.rb
+++ bf_interpreter.rb
@@ -5,14 +5,25 @@ class Brainfuck
     @saddr = 0
     @maddr = 0
     @script = src.gsub(/\r\n|\r|\n|\s|\t/, "")
-    @mem = Array.new(1000)
+    @mem = Array.new(100,0)
   end
 
   def read()
     ord = @script[@saddr]
     return if ord.nil?
     @saddr+=1
-    print ord
+
+    # > < + - . , [ ]
+    case ord
+    when ">"
+      @maddr+=1
+    when "<"
+      @maddr-=1
+    when "+"
+      @mem[@maddr]+=1
+    when "-"
+      @mem[@maddr]-=1
+    end
     read()
   end
 end
