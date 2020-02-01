e8d03cf85d72b1dc926811dc504dacc71060d0d5
nos <nos@kmc.gr.jp>
2020-02-02 08:46:06 +0900

    read()

--- bf_interpreter.rb
+++ bf_interpreter.rb
@@ -4,7 +4,28 @@ class Brainfuck
   def initialize(src)
     @saddr = 0
     @maddr = 0
-    @script = src
-    @mem = []
+    @script = src.gsub(/\r\n|\r|\n|\s|\t/, "")
+    @mem = Array.new(1000)
+  end
+
+  def read()
+    ord = @script[@saddr]
+    return if ord.nil?
+    @saddr+=1
+    print ord
+    read()
   end
 end
+
+
+# print hoge
+s="++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++
+++++++++.
++++++++.
+--------.
+--."
+
+bf = Brainfuck.new(s)
+bf.read()
